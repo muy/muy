1a02f95038469f5c3c09263d08892e03cbc5ae6c
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-03-14 13:40:30 -0300

    feat(Blockquote): add Blockquote (just for experimenting)

--- packages/muy/src/index.js
+++ packages/muy/src/index.js
@@ -1,1 +1,15 @@
-export default 'muy'
+import React from 'react'
+import makeStyles from "@material-ui/core/styles/makeStyles";
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        padding: 0
+    }
+}))
+
+const Blockquote = (props) => {
+    const classes = useStyles()
+    return <blockquote className={classes.root} {...props} />
+}
+
+export default Blockquote
