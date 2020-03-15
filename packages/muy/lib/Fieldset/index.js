ab7a2ec87460307c23aa035246b646ca35825bd5
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-03-15 02:25:13 -0300

    fix(Fieldset): fix support to borderStyle CSS property

--- packages/muy/lib/Fieldset/index.js
+++ packages/muy/lib/Fieldset/index.js
@@ -1,20 +1,44 @@
 import React from "react"
 import Box from "@material-ui/core/Box"
+import clsx from "clsx"
+import makeStyles from "@material-ui/core/styles/makeStyles"
 
-const Fieldset = ({ variant, ...props }) => (
-    <Box
-        border={variant === "borderSolid" ? 0 : 1}
-        borderColor={variant === "borderSolid" && "divider"}
-        borderStyle={variant === "borderSolid" ? "solid" : "none"}
-        component="fieldset"
-        mb={3}
-        mr={0}
-        mt={0}
-        pb={variant === "borderNone" ? 0 : 1.25}
-        pr={variant === "borderNone" ? 0 : 1}
-        pt={variant === "borderNone" ? 0 : 0.5}
-        {...props}
-    />
-)
+/**
+ * @todo Migrate some Fieldset styles to Box properties
+ * @type {(props?: any) => ClassNameMap<string>}
+ */
+const useStyles = makeStyles({
+    borderNone: {
+        borderStyle: 'none'
+    },
+    borderSolid: {
+        borderStyle: 'solid'
+    }
+})
+
+const Fieldset = ({ className, variant, ...props }) => {
+    const classes = useStyles()
+    return (
+        <Box
+            border={variant === "borderSolid" ? 0 : 1}
+            borderColor={variant === "borderSolid" && "divider"}
+            borderStyle={variant === "borderSolid" ? "solid" : "none"}
+            className={clsx(
+                classes,
+                className,
+                variant === "borderNone" && classes.borderNone,
+                variant === "borderSolid" && classes.borderSolid
+            )}
+            component="fieldset"
+            mb={3}
+            mr={0}
+            mt={0}
+            pb={variant === "borderNone" ? 0 : 1.25}
+            pr={variant === "borderNone" ? 0 : 1}
+            pt={variant === "borderNone" ? 0 : 0.5}
+            {...props}
+        />
+    )
+}
 
 export default Fieldset
