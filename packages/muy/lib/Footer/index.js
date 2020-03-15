e39ece05339ed78d5793b798f7079977e2dd6c0a
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-03-15 01:27:18 -0300

    feat(Footer): add support to display property, "block" by default

--- packages/muy/lib/Footer/index.js
+++ packages/muy/lib/Footer/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Box from "@material-ui/core/Box";
 
-const Footer = (props) => <Box component="footer" {...props} />
+const Footer = ({display = 'block', ...props}) => <Box component="footer" display={display} {...props} />
 
 export default Footer
