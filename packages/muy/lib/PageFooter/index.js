9091c236e0602c951c7b339f0d072409ddab356d
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-03-15 00:36:00 -0300

    feat(PageFooter): add component

---
+++ packages/muy/lib/PageFooter/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import Footer from "../Footer";
+
+const PageFooter = (props) => <Footer itemScope itemType="https://schema.org/WPFooter" {...props} />
+
+export default PageFooter
