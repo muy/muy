976e615ed8df040336f9d83acb791f5f3eebd1e7
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-03-15 00:36:15 -0300

    feat(lib): export new components

--- packages/muy/lib/index.js
+++ packages/muy/lib/index.js
@@ -4,11 +4,15 @@ import EmbedGoogleMap from "./EmbedGoogleMap";
 import Fieldset from "./Fieldset";
 import Figure from "./Figure";
 import FileInput from "./FileInput";
+import Footer from "./Footer";
 import Form from "./Form";
+import Header from "./Header";
 import HorizontalList from "./HorizontalList";
 import Iframe from "./Iframe";
 import Image from "./Image";
 import MainContent from "./MainContent";
+import PageFooter from "./PageFooter";
+import PageHeader from "./PageHeader";
 
 export {
     Blockquote,
@@ -17,9 +21,13 @@ export {
     Fieldset,
     Figure,
     FileInput,
+    Footer,
     Form,
+    Header,
     HorizontalList,
     Iframe,
     Image,
-    MainContent
+    MainContent,
+    PageFooter,
+    PageHeader
 }
