15bc91b62be253ecb21f555bd32b304efaa6eff0
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-03-19 11:35:16 -0300

    chore(__tests__): remove tests folder

--- packages/muy/__tests__/Blockquote/index.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Blockquote from "../../lib/Blockquote";
-import rendersCorrectly from "../../test-utils/rendersCorrectly";
-
-describe('<Blockquote />', () => {
-    it("renders correctly", () => {
-        rendersCorrectly(<Blockquote/>)
-    })
-})
