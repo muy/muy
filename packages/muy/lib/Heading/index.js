5dc1144aff4b5ceef151433d045b235d7d8b8bb5
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-03-20 20:15:22 -0300

    feat(Heading): close #11 - add support to `variant` prop

--- packages/muy/lib/Heading/index.js
+++ packages/muy/lib/Heading/index.js
@@ -1,21 +1,72 @@
 import React from "react"
+import PropTypes from "prop-types"
 import Typography from "@material-ui/core/Typography"
 
-export const defaultProps = {
+export const getHnDefaultProps = level => ({
+  variant: `h${level}`,
+})
+
+const Heading = ({ level, variant, ...props }) => (
+  <Typography
+    component={`h${level}`}
+    variant={variant ? variant : getHnDefaultProps(level).variant}
+    {...props}
+  />
+)
+
+const basePropTypes = {
+  variant: PropTypes.string,
+}
+
+Heading.propTypes = {
+  level: PropTypes.oneOf([1, 2, 3, 4, 5, 6]),
+  ...basePropTypes,
+}
+
+Heading.defaultProps = {
   level: 1,
 }
 
-const Heading = ({ level = defaultProps.level, ...props }) => (
-  <Typography component={`h${level}`} variant={`h${level}`} {...props} />
-)
-
 export default Heading
 
-Heading.defaultProps = defaultProps
+export const H1 = ({ variant = getHnDefaultProps(1), ...props }) => (
+  <Heading level={1} variant={variant} {...props} />
+)
 
-export const H1 = props => <Heading level={1} {...props} />
-export const H2 = props => <Heading level={2} {...props} />
-export const H3 = props => <Heading level={3} {...props} />
-export const H4 = props => <Heading level={4} {...props} />
-export const H5 = props => <Heading level={5} {...props} />
-export const H6 = props => <Heading level={6} {...props} />
+export const H2 = ({ variant = getHnDefaultProps(2), ...props }) => (
+  <Heading level={2} variant={variant} {...props} />
+)
+
+export const H3 = ({ variant = getHnDefaultProps(3), ...props }) => (
+  <Heading level={3} variant={variant} {...props} />
+)
+
+export const H4 = ({ variant = getHnDefaultProps(4), ...props }) => (
+  <Heading level={4} variant={variant} {...props} />
+)
+
+export const H5 = ({ variant = getHnDefaultProps(5), ...props }) => (
+  <Heading level={5} variant={variant} {...props} />
+)
+
+export const H6 = ({ variant = getHnDefaultProps(6), ...props }) => (
+  <Heading level={6} variant={variant} {...props} />
+)
+
+H1.propTypes = basePropTypes
+H1.defaultProps = getHnDefaultProps(1)
+
+H2.propTypes = basePropTypes
+H2.defaultProps = getHnDefaultProps(2)
+
+H3.propTypes = basePropTypes
+H3.defaultProps = getHnDefaultProps(3)
+
+H4.propTypes = basePropTypes
+H4.defaultProps = getHnDefaultProps(4)
+
+H5.propTypes = basePropTypes
+H5.defaultProps = getHnDefaultProps(5)
+
+H6.propTypes = basePropTypes
+H6.defaultProps = getHnDefaultProps(6)
