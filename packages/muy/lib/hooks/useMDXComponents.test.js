16aef93817101ac0902456abce68312dbc9949c5
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-03-21 12:38:24 -0300

    test(useMDXComponents): bring changes proposed by @viniciusponciano-tw
    Bring changes proposed by @viniciusponciano-tw at https://github.com/educodar/web/pull/250
    
    Co-Authored-By: Vinícius Ponciano <viniciusponciano-tw@users.noreply.github.com>

--- packages/muy/lib/hooks/useMDXComponents.test.js
+++ packages/muy/lib/hooks/useMDXComponents.test.js
@@ -1,24 +1,36 @@
-import React from "react"
 import useMDXComponents from "./useMDXComponents"
-import rendersCorrectly from "../../test-utils/rendersCorrectly"
 
-describe("useMDXComponent hook", () => {
-  const levels = [1, 2, 3, 4, 5, 6]
+describe("Use MDX components hook", () => {
+  let components
 
-  describe.each(levels)(`H%d component`, level => {
-    let components
+  beforeAll(() => {
+    components = useMDXComponents()
+  })
 
-    beforeAll(() => {
-      components = useMDXComponents()
-    })
+  it("should return an object", () => {
+    expect(components).toBeInstanceOf(Object)
+  })
 
-    it("should be defined", () => {
-      expect(components[`h${level}`]).toBeDefined()
-    })
+  const expectedProperties = [
+    "a",
+    "blockquote",
+    "h1",
+    "h2",
+    "h3",
+    "h4",
+    "h5",
+    "h6",
+    "hr",
+    "img",
+    "li",
+    "p",
+    "table",
+    "td",
+    "tr",
+    "ul",
+  ]
 
-    it("should render correctly", () => {
-      const Component = `h${level}`
-      rendersCorrectly(<Component />)
-    })
+  it.each(expectedProperties)("should define component for %p", property => {
+    expect(components).toHaveProperty(property)
   })
 })
