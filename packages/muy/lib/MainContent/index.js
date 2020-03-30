d8a78fc72673b89ea3c1fc0ee6a3930f205c17d5
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-03-30 19:51:36 -0300

    chore(MainContent): add default id prop

--- packages/muy/lib/MainContent/index.js
+++ packages/muy/lib/MainContent/index.js
@@ -1,8 +1,26 @@
 import React from "react"
+import PropTypes from "prop-types"
 import Box from "@material-ui/core/Box"
 
-const MainContent = props => (
-  <Box component="main" display="block" mt={3} role="main" {...props} />
+const MainContent = ({ id, mt, ...props }) => (
+  <Box
+    id={id}
+    component="main"
+    display="block"
+    mt={mt}
+    role="main"
+    {...props}
+  />
 )
 
 export default MainContent
+
+MainContent.defaultProps = {
+  id: "content",
+  mt: 3,
+}
+
+MainContent.propTypes = {
+  id: PropTypes.string,
+  mt: PropTypes.number,
+}
