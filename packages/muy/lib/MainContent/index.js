0011925a152a945b60cf5599e86754b287ccac84
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-04-02 01:52:46 -0300

    fix(MainContent): remove outline on focus

--- packages/muy/lib/MainContent/index.js
+++ packages/muy/lib/MainContent/index.js
@@ -1,18 +1,31 @@
 import React from "react"
 import PropTypes from "prop-types"
 import Box from "@material-ui/core/Box"
+import makeStyles from "@material-ui/core/styles/makeStyles"
 
-const MainContent = ({ id, mt, ...props }) => (
-  <Box
-    id={id}
-    component="main"
-    display="block"
-    mt={mt}
-    role="main"
-    tabIndex={-1}
-    {...props}
-  />
-)
+const useStyles = makeStyles({
+  root: {
+    outlineColor: "transparent",
+    outlineStyle: "none",
+    outlineWidth: 0,
+  },
+})
+
+const MainContent = ({ id, mt, ...props }) => {
+  const classes = useStyles()
+  return (
+    <Box
+      className={classes.root}
+      id={id}
+      component="main"
+      display="block"
+      mt={mt}
+      role="main"
+      tabIndex={-1}
+      {...props}
+    />
+  )
+}
 
 export default MainContent
 
