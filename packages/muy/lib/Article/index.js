569aa854a2d112328af9eb8db352b42aaa2dc362
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-04-24 12:06:25 -0300

    docs(Article): add jsdoc and propTypes

--- packages/muy/lib/Article/index.js
+++ packages/muy/lib/Article/index.js
@@ -1,6 +1,28 @@
 import React from "react"
+import PropTypes from "prop-types"
 import Box from "@material-ui/core/Box"
 
-const Article = props => <Box component="article" display="block" {...props} />
+/**
+ * Muy Article component
+ *
+ * @param display
+ * @param props
+ * @return {*}
+ * @constructor
+ */
+const Article = ({ display, ...props }) => (
+  <Box component="article" display={display} {...props} />
+)
 
 export default Article
+
+Article.defaultProps = {
+  display: "block",
+}
+
+Article.propTypes = {
+  display: PropTypes.oneOfType([
+    PropTypes.object,
+    PropTypes.oneOf(["block", "inline"]),
+  ]),
+}
