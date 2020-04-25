c35b014eac496d1ed63ef2f1aa6f983794abb090
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-04-24 21:47:47 -0300

    test(DateField): add one more snapshot test

--- packages/muy/lib/inputFields/DateField/index.test.js
+++ packages/muy/lib/inputFields/DateField/index.test.js
@@ -3,7 +3,10 @@ import DateField from "."
 import rendersCorrectly from "../../../test-utils/rendersCorrectly"
 
 describe("<DateField /> component", () => {
-  it("renders correctly", () => {
+  it("renders correctly with no props", () => {
     rendersCorrectly(<DateField />)
+  })
+  it("renders correctly with label prop", () => {
+    rendersCorrectly(<DateField label={"Date Label Text"} />)
   })
 })
