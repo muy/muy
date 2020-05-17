66f9a8314bfbdee21cb518352986a0939168dfbc
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-05-17 14:28:49 -0300

    fix(*Header) : forward ref

--- packages/muy/lib/Header/index.js
+++ packages/muy/lib/Header/index.js
@@ -1,6 +1,10 @@
 import React from "react"
 import Box from "@material-ui/core/Box"
 
-const Header = props => <Box component="header" display="block" {...props} />
+const PureHeader = (props, ref) => (
+  <Box component="header" display="block" ref={ref} {...props} />
+)
+
+const Header = React.forwardRef(PureHeader)
 
 export default Header
--- packages/muy/lib/PageHeader/index.js
+++ packages/muy/lib/PageHeader/index.js
@@ -1,13 +1,16 @@
 import React from "react"
 import Header from "../Header"
 
-const PageHeader = props => (
+const PurePageHeader = (props, ref) => (
   <Header
     itemScope
     itemType="https://schema.org/WPHeader"
+    ref={ref}
     role="banner"
     {...props}
   />
 )
+
+const PageHeader = React.forwardRef(PurePageHeader)
 
 export default PageHeader
